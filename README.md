37d5adcca8a7c569f8fdc96634b966d9f8745627
Cees Timmerman <CTimmerman@users.noreply.github.com>
2021-07-12 13:15:00 +0200

    Add math functions like in my SymPyServer demo.

--- README.md
+++ README.md
@@ -5,21 +5,25 @@ Simple calculator JSON REST API.
 
 Valid equations return code 200 and the result:
 
-    http://localhost:8888/?q={%22q%22:%221%20-%202%20/%206%22}
+    http://localhost:8888/?q={"q":"sqrt(e)-log(pi)"}
+
+    {"result": 0.503991384850728}
+
+    http://localhost:8888/?q={"q":"1 - 2 / 6"}
+
+    {"result": 0.6666666666666667}
+
+JSON via POST is easier via the terminal:
+
+    curl 127.0.0.1:8888 -d {"q":"1 - 2 / 6"}
 
     {"result": 0.6666666666666667}
 
 Invalid equations return code 400 and the reason as result:
 
-    http://localhost:8888/?q={%22q%22:%221%20-%202%20/%20b6%22}
+    http://localhost:8888/?q={"q":"1 - 2 / b6"}
 
-    {"result": "Invalid equation. Characters should match ^[ 0-9.()^|*/%+-]*$."}
-
-JSON via POST is easier via the terminal:
-
-    curl 127.0.0.1:8888 -d '{"q": "1+1"}'
-
-    {"result": 2}
+    {"result": "Invalid equation. Should match ^([ 0-9.()^|*/%+-]|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|comb|copysign|cos|cosh|degrees|dist|e|erf|erfc|exp|expm1|fabs|factorial|floor|fmod|frexp|fsum|gamma|gcd|hypot|inf|isclose|isfinite|isinf|isnan|isqrt|ldexp|lgamma|log|log10|log1p|log2|modf|nan|perm|pi|pow|prod|radians|remainder|sin|sinh|sqrt|tan|tanh|tau|trunc)+$."}
 
 ## Installation
 
--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 """A simple calculator JSON REST API."""
 
-import json, re
+import json, logging, math, re
 from wsgiref.simple_server import make_server
+
 from pyramid.config import Configurator
 
 
-VALID_PATTERN = "^[ 0-9.()^|*/%+-]*$"
+VALID_PATTERN = (
+    f"^([ 0-9.()^|*/%+-]|{'|'.join(name for name in dir(math) if '_' not in name)})+$"
+)
+PORT = 8888
 
 
 def calc(request):
+    """
+    >>> from pyramid.testing import DummyRequest
+    >>> calc(DummyRequest(json_body={'q':'pi / (1+1)'}, method='POST')).body
+    b'{"result": 1.5707963267948966}'
+    >>> calc(DummyRequest(json_body={'q':'pi / (1+1) / hax'}, method='POST')).status
+    '400 Bad Request'
+    """
     try:
         q_param = request.params.get("q")
         if q_param:
             equation = json.loads(q_param)["q"]
         else:
             equation = request.json_body["q"]
-        print(f"Equation: {equation}")
+        logging.debug(f"Equation: {equation}")
         if re.match(VALID_PATTERN, equation):
-            result = eval(equation)
+            result = eval(equation, math.__dict__)
         else:
-            result = f"Invalid equation. Characters should match {VALID_PATTERN}."
+            result = f"Invalid equation. Should match {VALID_PATTERN}."
             request.response.status = 400
     except Exception as e:
         result = json.dumps(str(e))
         request.response.status = 400
 
-    print(result)
+    logging.debug(result)
     request.response.body = json.dumps({"result": result}).encode("utf8")
     request.response.content_type = "application/json"
     return request.response
@@ -36,5 +47,6 @@ if __name__ == "__main__":
         config.add_route("calc", "/")
         config.add_view(calc, route_name="calc")
         app = config.make_wsgi_app()
-    server = make_server("0.0.0.0", 8888, app)
+    server = make_server("0.0.0.0", PORT, app)
+    logging.debug(f"Listening on {PORT} for {VALID_PATTERN}")
     server.serve_forever()
